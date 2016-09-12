183855bedd4415a86b565d1fcebb21345c1aeacb
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2016-09-12 16:19:06 +0900

    Count up on task added

---
+++ ToDoApp.Test/TodoTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ToDoApp.Test
+{
+    [TestClass]
+    public class タスク件数
+    {
+        [TestMethod]
+        public void 初期状態ではタスクは0件()
+        {
+            var todo = new ToDo();
+            int count = todo.Count;
+            Assert.AreEqual(0, count);
+        }
+
+        [TestMethod]
+        public void タスクを追加したら1件()
+        {
+            var todo = new ToDo();
+            todo.AddTask("x");
+            int count = todo.Count;
+            Assert.AreEqual(1, count);
+        }
+    }
+}
---
+++ ToDoApp/ToDo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoApp
+{
+    public class ToDo
+    {
+        private List<string> tasks = new List<string>();
+
+        public int Count
+        {
+            get
+            {
+                return tasks.Count;
+            }
+        }
+
+        public ToDo()
+        {
+        }
+
+        public void AddTask(string taskText)
+        {
+            tasks.Add(taskText);
+        }
+    }
+}
